7f0cdd66840a2dbbed6677e6470f5fd0000e7872
Finderchangchang <1031066280@qq.com>
2015-10-23 15:38:30 +0800

    第一次提交123

---
+++ custom/src/main/java/liuliu/custom/spinner/popsipnner/NormalSpinerAdapter.java
@@ -0,0 +1,10 @@
+package liuliu.custom.spinner.popsipnner;
+
+import android.content.Context;
+
+public class NormalSpinerAdapter extends AbstractSpinerAdapter<String>{
+
+	public  NormalSpinerAdapter(Context context){
+		super(context);
+	}
+}
