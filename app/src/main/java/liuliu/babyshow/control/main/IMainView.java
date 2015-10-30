6f732e3f82aee733094963da34a8135e79705a22
Finderchangchang <1031066280@qq.com>
2015-10-30 08:56:22 +0800

    6666

---
+++ app/src/main/java/liuliu/babyshow/control/main/IMainView.java
@@ -0,0 +1,11 @@
+package liuliu.babyshow.control.main;
+
+/**
+ * Created by liuliu on 2015/10/29   16:00
+ * 主页面消息处理
+ *
+ * @author 柳伟杰
+ * @Email 1031066280@qq.com
+ */
+public interface IMainView {
+}
---
+++ app/src/main/java/liuliu/babyshow/control/main/MainListener.java
@@ -0,0 +1,14 @@
+package liuliu.babyshow.control.main;
+
+/**
+ * Created by liuliu on 2015/10/29   16:01
+ * 主页面Listener处理
+ *
+ * @author 柳伟杰
+ * @Email 1031066280@qq.com
+ */
+public class MainListener {
+    public MainListener() {
+
+    }
+}
